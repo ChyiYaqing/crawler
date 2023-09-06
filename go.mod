9ca34ef599365be007dded985362f3311925682d
Yaqing QI <qiyaqing@hyperchain.cn>
2023-09-06 16:37:28 +0800

    regex

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/chyiyaqing/crawler
 
 go 1.20
+
+require (
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,33 +1,73 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"net/http"
+	"regexp"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
+
+// [\s\S] 指代的是任意字符串
+// * 代表将前面任意字符匹配0次或者无数次
+// ? 代表非贪婪匹配
+var headerRe = regexp.MustCompile(`<div class="small_cardcontent__BTALp"[\s\S]*?<h2>([\s\S]*?)</h2>`)
 
 func main() {
 	url := "https://www.thepaper.cn/"
-	resp, err := http.Get(url)
-
-	if err != nil {
-		fmt.Printf("fetch url error:%v\n", err)
-		return
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code:%v\n", resp.StatusCode)
-		return
-	}
-
-	body, err := io.ReadAll(resp.Body)
+	body, err := Fetch(url)
 
 	if err != nil {
 		fmt.Printf("read content failed:%v\n", err)
 		return
 	}
 
-	fmt.Println("body:", string(body))
+	matches := headerRe.FindAllSubmatch(body, -1)
+	for _, m := range matches {
+		fmt.Println("fetch card news:", string(m[1]))
+	}
+
+	// // 统计<a> 标签的数量
+	// numLinks := strings.Count(string(body), "<a")
+	// fmt.Printf("homepage has %d links!\n", numLinks)
+
+	// // 是否存在相关关键字
+	// exists := strings.Contains(string(body), "运动")
+	// fmt.Printf("是否存在足球:%v\n", exists)
+}
+
+func Fetch(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code:%v\n", resp.StatusCode)
+	}
+
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return io.ReadAll(utf8Reader)
+}
+
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+
+	if err != nil {
+		fmt.Println("fetch error:%v", err)
+		return unicode.UTF8
+	}
+
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
