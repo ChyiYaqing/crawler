d9c50f479867549b471ac2bb067cd93595241394
Yaqing QI <qiyaqing@hyperchain.cn>
2023-09-06 17:20:12 +0800

    四种网页文本处理手段

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/chyiyaqing/crawler
 go 1.20
 
 require (
-	golang.org/x/net v0.15.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	github.com/PuerkitoBio/goquery v1.8.1
+	golang.org/x/net v0.15.0
+	golang.org/x/text v0.13.0
 )
+
+require github.com/andybalholm/cascadia v1.3.1 // indirect
--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
-	"regexp"
 
+	"github.com/PuerkitoBio/goquery"
 	"golang.org/x/net/html/charset"
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/unicode"
@@ -16,7 +17,7 @@ import (
 // [\s\S] 指代的是任意字符串
 // * 代表将前面任意字符匹配0次或者无数次
 // ? 代表非贪婪匹配
-var headerRe = regexp.MustCompile(`<div class="small_cardcontent__BTALp"[\s\S]*?<h2>([\s\S]*?)</h2>`)
+//var headerRe = regexp.MustCompile(`<div class="small_cardcontent__BTALp"[\s\S]*?<h2>([\s\S]*?)</h2>`)
 
 func main() {
 	url := "https://www.thepaper.cn/"
@@ -27,18 +28,18 @@ func main() {
 		return
 	}
 
-	matches := headerRe.FindAllSubmatch(body, -1)
-	for _, m := range matches {
-		fmt.Println("fetch card news:", string(m[1]))
+	// 加载HTML文档
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
+	if err != nil {
+		fmt.Println("read content failed:%v", err)
 	}
 
-	// // 统计<a> 标签的数量
-	// numLinks := strings.Count(string(body), "<a")
-	// fmt.Printf("homepage has %d links!\n", numLinks)
+	doc.Find("div.small_cardcontent__BTALp a[target=_blank] h2").Each(func(i int, s *goquery.Selection) {
+		// 获取匹配标签中的文本
+		title := s.Text()
+		fmt.Printf("Review %d: %s\n", i, title)
+	})
 
-	// // 是否存在相关关键字
-	// exists := strings.Contains(string(body), "运动")
-	// fmt.Printf("是否存在足球:%v\n", exists)
 }
 
 func Fetch(url string) ([]byte, error) {
